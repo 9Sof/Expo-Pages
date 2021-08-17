f7cb2be03abaf36d1afa79d3f961a62844b4a45b
9Sof <sopee_namyen@hotmail.com>
2021-08-17 13:30:17 +0700

    component pages folder

---
+++ components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { View,StyleSheet,Text } from 'react-native'
+
+const Header = ({title}) => {
+    return (
+        <View style={styles.header}>
+            <Text style={styles.title}>{title}</Text>
+        </View>
+    )
+}
+
+const styles= StyleSheet.create({
+    header:{
+        width: '100%',
+        height: 50,
+        backgroundColor: '#f7287b',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    title : {
+        fontSize: 20,
+        color: '#fff',
+        fontWeight: 'bold',
+    }
+})
+
+export default Header
