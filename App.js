be97e40481d4192e7da9a70e22e2e627875a26b6
9Sof <sopee_namyen@hotmail.com>
2021-08-18 12:16:01 +0700

    Platform select

--- App.js
+++ App.js
@@ -91,7 +91,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: Platform.OS === "android" ? "white" : "red",
+    // backgroundColor: Platform.OS === "android" ? "white" : "red",
+    backgroundColor: Platform.select({
+      android: "white",
+      ios: "red",
+    }),
   },
   screen: {
     flex: 1,
